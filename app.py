18398340d44c1bc3a7b8d8e191bf055da9ec1e25
Jedediah Arnold <jarnold910@gmail.com>
2020-09-02 19:27:19 -0400

    install WTForms, add user model to the database

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db, connect_db
 
 app = Flask(__name__)
 # ADD YOUR DB HERE 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config["SECRET_KEY"] = "hihihi333"
--- models.py
+++ models.py
@@ -8,3 +8,19 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
+
+    password = db.Column(db.Text, nullable=False)
+
+    email = db.Column(db.String(50), nullable=False)
+
+    first_name = db.Column(db.String(30), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
